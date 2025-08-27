86d745a50479fe4324f55e9781890f42677ead8e
jamelclyons <jamel.c.lyons@gmail.com>
2025-08-27 06:56:49 -0400

    Refactoring 2025-08-27 @ 06:56:49

--- src/views/components/skills/SkillsComponent.tsx
+++ src/views/components/skills/SkillsComponent.tsx
@@ -14,12 +14,20 @@ export const SkillsComponent: React.FC<SkillsComponentProps> = ({ skills }) => {
 
     return (
         <>
-            {skills && (skills.types || skills.languages || skills.frameworks || skills.technologies || skills.services) &&
+            {skills && (skills.types || skills.softwareApplications || skills.databases || skills.languages || skills.frameworks || skills.technologies || skills.cicdTools || skills.platforms || skills.cloudProviders) &&
                 <div className={styles.skills} id="skills">
                     <h3 className={styles.title}>skills</h3>
 
                     {skills.types.size > 0 && (
                         <TaxListIcon taxonomiesSet={skills.types} taxonomiesTitle="Project Types" handleClick={handleSkillClick} />
+                    )}
+
+                    {skills.softwareApplications.size > 0 && (
+                        <TaxListIcon taxonomiesSet={skills.softwareApplications} taxonomiesTitle="Software" handleClick={handleSkillClick} />
+                    )}
+
+                    {skills.databases.size > 0 && (
+                        <TaxListIcon taxonomiesSet={skills.databases} taxonomiesTitle="Databases" handleClick={handleSkillClick} />
                     )}
 
                     {skills.languages.size > 0 && (
@@ -34,8 +42,24 @@ export const SkillsComponent: React.FC<SkillsComponentProps> = ({ skills }) => {
                         <TaxListIcon taxonomiesSet={skills.technologies} taxonomiesTitle="Technologies" handleClick={handleSkillClick} />
                     )}
 
-                    {skills.services.size > 0 && (
-                        <TaxListIcon taxonomiesSet={skills.services} taxonomiesTitle="Services" handleClick={handleSkillClick} />
+                    {skills.buildTools.size > 0 && (
+                        <TaxListIcon taxonomiesSet={skills.buildTools} taxonomiesTitle="Build Tools" handleClick={handleSkillClick} />
+                    )}
+
+                    {skills.servers.size > 0 && (
+                        <TaxListIcon taxonomiesSet={skills.servers} taxonomiesTitle="Servers" handleClick={handleSkillClick} />
+                    )}
+
+                    {skills.cicdTools.size > 0 && (
+                        <TaxListIcon taxonomiesSet={skills.cicdTools} taxonomiesTitle="CI/CD" handleClick={handleSkillClick} />
+                    )}
+
+                    {skills.platforms.size > 0 && (
+                        <TaxListIcon taxonomiesSet={skills.platforms} taxonomiesTitle="Platforms" handleClick={handleSkillClick} />
+                    )}
+
+                    {skills.cloudProviders.size > 0 && (
+                        <TaxListIcon taxonomiesSet={skills.cloudProviders} taxonomiesTitle="Cloud Providers" handleClick={handleSkillClick} />
                     )}
                 </div>
             }
